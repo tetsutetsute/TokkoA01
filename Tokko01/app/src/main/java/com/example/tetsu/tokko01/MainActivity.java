226ef887fb2c3ad3db1c9d09d6334b9030e53153
S.Tetsu <tetsuhiko.sapporo@gmail.com>
2019-07-24 23:42:50 +0900

    追加； ・判定音 ・判定メッセージ ・各コメント

--- Tokko01/app/src/main/java/com/example/tetsu/tokko01/MainActivity.java
+++ Tokko01/app/src/main/java/com/example/tetsu/tokko01/MainActivity.java
@@ -13,10 +13,12 @@ import android.net.*;
 import android.os.*;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.*;
 import android.graphics.*;
 import android.widget.*;
 import android.provider.*;
+import android.media.AudioAttributes;
 
 import com.microsoft.projectoxford.face.*;
 import com.microsoft.projectoxford.face.contract.*;
@@ -27,20 +29,22 @@ public class MainActivity extends AppCompatActivity {
     private ProgressDialog detectionProgressDialog;
 
 
-    //private final String apiEndpoint = "<endpoint>";
-    //↑の<endpoint>部分をWebAPIのURLに置き換える↓
+    //private final String apiEndpoint = "WebAPIのURL";
     private final String apiEndpoint = "https://westcentralus.api.cognitive.microsoft.com/face/v1.0";
 
-    //private final String subscriptionKey = "<subscriptionKey>";
-    //↑の<subscriptionKey>をmicrosoftから指定されたkey(暗号みたいなもん)に置き換える↓
-//    private final String subscriptionKey = "5ad1ceacf92b4611bfebfb5a76738485";
+    //private final String subscriptionKey = "microsoftから指定されたsubscriptionKey";
     private final String subscriptionKey = "1b4e1d4b1ec047229d3ea6bc547216d2";
 
+    //faceServiceClient→マイクロソフト様がおつくりになられたWebAPI職人クラス。
     private final FaceServiceClient faceServiceClient =
             new FaceServiceRestClient(apiEndpoint, subscriptionKey);
 
+    //音を出すクラス
     private SoundPool soundPool;
+    //音のID
+    private int sound_correct,sound_false;
 
+    //撮った画像を保存する先（スマホ内）
     private Uri _imageUri;
     private final String _imageFileName = "Tokko01.jpg";
 
@@ -49,28 +53,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //↑この2行はほぼテンプレで、最初から書いてある。
 
-//        Button button1 = findViewById(R.id.button1);
-//        button1.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-//                intent.setType("image/*");
-//                startActivityForResult(Intent.createChooser(
-//                        intent, "Select Picture"), PICK_IMAGE);
-//            }
-//        });
-
-        //プログレス（進捗）表示はいらんかな。
+        //プログレス（進捗）表示
         detectionProgressDialog = new ProgressDialog(this);
 
-        soundPool = new SoundPool(5, AudioManager.STREAM_MUSIC, 0);
+        //効果音初期化
+        initSoundPool();
     }
 
     //カメラびゅう（つまり画面）クリック時の動作
     //びゅうはapp/res/layout/activity_main.xmlで初期化、設定してるよ。
     public void onCameraImageClick(View view) {
-
+        //アプリからスマホへ、ストレージをいじる許可を確認して、ない場合は取得
         if(ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             String[] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
             ActivityCompat.requestPermissions(this, permissions, 2000);
@@ -87,6 +82,7 @@ public class MainActivity extends AppCompatActivity {
         //スマホのキャプチャ(カメラ)機能(暗黙intent)
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
+        //スマホのストレージに保存することで画像が小さくならない
         intent.putExtra(MediaStore.EXTRA_OUTPUT, _imageUri);
 
         //intentをリクエストコード:200で呼び出し。200っていうのを下のonActibityResultでつかう。
@@ -98,23 +94,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        //マイクロソフトのテンプレ部分コメントアウト
-//        if (requestCode == PICK_IMAGE && resultCode == RESULT_OK &&
-//                data != null && data.getData() != null) {
-//            Uri uri = data.getData();
-//            try {
-//                Bitmap bitmap = MediaStore.Images.Media.getBitmap(
-//                        getContentResolver(), uri);
-//                ImageView imageView = findViewById(R.id.imageView1);
-//                imageView.setImageBitmap(bitmap);
-//
-//                // Comment out for tutorial
-//                detectAndFrame(bitmap);
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        }
-
         //リクエストコード：200のとき！
         if (requestCode == 200 && resultCode == RESULT_OK) {
             try {
@@ -125,7 +104,9 @@ public class MainActivity extends AppCompatActivity {
                 ImageView imageView = findViewById(R.id.ivCamera);
                 imageView.setImageBitmap(bitmap);
 
+                //これぞ核となる顔認識のメソッドである。
                 detectAndFrame(bitmap);
+
             }catch (Exception e){
                 e.printStackTrace();
             }
@@ -133,7 +114,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
+    //これぞ核となる顔認識のメソッドである。
     private void detectAndFrame(final Bitmap imageBitmap) {
         //streamは基礎知識なので調べといて。
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
@@ -142,6 +123,7 @@ public class MainActivity extends AppCompatActivity {
         ByteArrayInputStream inputStream =
                 new ByteArrayInputStream(outputStream.toByteArray());
 
+        //Async（非同期）Task（処理）。非同期処理なのだ。別スレッドで動くのだ。
         AsyncTask<InputStream, String, Face[]> detectTask =
                 new AsyncTask<InputStream, String, Face[]>() {
                     String exceptionMessage = "";
@@ -149,15 +131,14 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     protected Face[] doInBackground(InputStream... params) {
                         try {
-                            publishProgress("Detecting...");
-                            //faceServiceClient→マイクロソフト様がおつくりになられたWebAPI職人クラス。
+                            publishProgress("AIが顔を見ています...");
+
                             Face[] result = faceServiceClient.detect(
                                     params[0],
                                     true,         // returnFaceId
                                     false,        // returnFaceLandmarks
-                                    //null          // returnFaceAttributes:
-                                    //顔の属性で受け取る項目を指定。
-                                    new FaceServiceClient.FaceAttributeType[] {
+                                     new FaceServiceClient.FaceAttributeType[] {
+                                             //受け取る顔の属性を指定
                                             //FaceServiceClient.FaceAttributeType.Age,
                                             //FaceServiceClient.FaceAttributeType.Gender,
                                             FaceServiceClient.FaceAttributeType.Emotion,
@@ -165,55 +146,74 @@ public class MainActivity extends AppCompatActivity {
                                     }
 
                             );
-                            //進捗報告はいらなそう。
+                            //進捗報告
                             if (result == null){
                                 publishProgress(
-                                        "Detection Finished. Nothing detected");
+                                        "誰もいない。。");
                                 return null;
                             }
                             publishProgress(String.format(
-                                    "Detection Finished. %d face(s) detected",
+                                    "%d人検知しました。 ",
                                     result.length));
 
 
                             return result;
                         } catch (Exception e) {
                             exceptionMessage = String.format(
-                                    "Detection failed: %s", e.getMessage());
+                                    "検知に失敗しました: %s", e.getMessage());
                             return null;
                         }
                     }
 
-//                    @Override
-//                    protected void onPreExecute() {
-//                        //TODO: show progress dialog
-//                        detectionProgressDialog.show();
-//                    }
-//                    @Override
-//                    protected void onProgressUpdate(String... progress) {
-//                        //TODO: update progress
-//                        detectionProgressDialog.setMessage(progress[0]);
-//                    }
+                    @Override
+                    protected void onPreExecute() {
+                        //プログレスダイアログ表示
+                        detectionProgressDialog.show();
+                    }
+                    @Override
+                    protected void onProgressUpdate(String... progress) {
+                        //TODO: update progress
+                        detectionProgressDialog.setMessage(progress[0]);
+                    }
                     @Override
                     protected void onPostExecute(Face[] result) {
-                        //TODO: update face frames
-                        //detectionProgressDialog.dismiss();
+
+                        //プログレスダイアログさようなら
+                        detectionProgressDialog.dismiss();
 
                         if(!exceptionMessage.equals("")){
-                            //うーん。
                             showError(exceptionMessage);
                         }
                         if (result == null) return;
 
                         for(Face face : result){
-                            if(face.faceAttributes.smile < 0.5){
-                                // 笑顔じゃないよ。
-                            }else{
-                                // 笑顔だよ。
-                                soundPool.play(R.raw.decision8, 1f, 1f, 0, 0, 1f);
+
+                            //笑顔ポイントを取得
+                            Double smilePoint = face.faceAttributes.smile;
+
+                            //笑顔ポイントによって判定
+                            if(smilePoint < 0.2){
+                                playJudgeSound(false);
+                                showSmileComment("笑顔ってなんだっけ");
+                            }else if(0.2 <= smilePoint){
+                                playJudgeSound(false);
+                                showSmileComment("笑顔かなぁ……");
+                            }
+                            else if(0.4 <= smilePoint){
+                                playJudgeSound(false);
+                                showSmileComment("ちょっと笑顔");
+                            }
+                            else if(0.6 <= smilePoint){
+                                playJudgeSound(true);
+                                showSmileComment("うん、笑顔");
+                            }
+                            else if(0.8 <= smilePoint){
+                                playJudgeSound(true);
+                                showSmileComment("誰もが認める笑顔");
                             }
                         }
 
+                        //撮った画像を画面に表示
                         ImageView imageView = findViewById(R.id.ivCamera);
                         imageView.setImageBitmap(
                                 drawFaceRectanglesOnBitmap(imageBitmap, result));
@@ -221,19 +221,47 @@ public class MainActivity extends AppCompatActivity {
                     }
                 };
 
+        //↑で定義した非同期処理を動かすのだ。
         detectTask.execute(inputStream);
     }
 
-    //エラー表示的なメソッド。いるかなあ。
+    //笑顔判定結果メッセージ表示
+    private void showSmileComment(String message){
+        new AlertDialog.Builder(this)
+                .setTitle("その表情は...")
+                .setMessage(message)
+                .create().show();
+    }
+
+    //エラー表示
     private void showError(String message) {
         new AlertDialog.Builder(this)
-                .setTitle("Error")
+                .setTitle("エラー")
                 .setMessage(message)
                 .setPositiveButton("OK", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
                     }})
                 .create().show();
     }
+
+    //判定音初期化(起動や再開時に呼び出しておくこと!)
+    private void initSoundPool(){
+        soundPool = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
+        sound_correct = soundPool.load(this,R.raw.correct3,1);
+        sound_false = soundPool.load(this,R.raw.incorrect1,1);
+    }
+
+    //判定音発射
+    private void playJudgeSound(boolean isCorrect){
+        int sound;
+        if(isCorrect){
+            sound = sound_correct;
+        }else{
+            sound = sound_false;
+        }
+        soundPool.play(sound,0.5f,0.5f,0,0,1);
+    }
+
 
     //画像の中の顔を四角で囲む。
     private static Bitmap drawFaceRectanglesOnBitmap(
